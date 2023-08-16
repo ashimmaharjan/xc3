c1b77342c8af72dc93fbb192e442c9fb2c0e01ba
Ankit <shrestha.ankit05@gmail.com>
2023-08-16 13:43:35 +1000

    change in project_spend_cost

--- src/budget_details/project_spend_cost.py
+++ src/budget_details/project_spend_cost.py
@@ -1,122 +1,87 @@
-# Copyright (c) 2023, Xgrid Inc, https://xgrid.co
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#        http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import datetime
-import botocore
 import logging
 import os
 from datetime import timedelta
-
 import boto3
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 import json
-
-try:
-    ec2_client = boto3.client("ec2")
-except Exception as e:
-    logging.error("Error creating boto3 client for ec2: " + str(e))
-try:
-    s3 = boto3.client("s3")
-except Exception as e:
-    logging.error("Error creating boto3 client for s3: " + str(e))
+# Initialize AWS clients
 try:
     ce_client = boto3.client("ce")
 except Exception as e:
     logging.error("Error creating boto3 client for ce: " + str(e))
-
-
+# Define the time period for cost data
 cost_by_days = 30
 end_date = str(datetime.datetime.now().date())
 start_date = str(datetime.datetime.now().date() - timedelta(days=cost_by_days))
-
-
 def cost_of_project(ce_client, start_date, end_date):
-    """
-    Calculates the cost of a project for a given time period.
-
-    Args:
-        ce_client (boto3.client): The AWS Cost Explorer client.
-        start_date (str): The start date of the time period in 'YYYY-MM-DD' format.
-        end_date (str): The end date of the time period in 'YYYY-MM-DD' format.
-
-    Returns:
-        dict: The cost of the project, grouped by project tag.
-    """
     try:
+        # Fetch cost and usage data from AWS Cost Explorer
         response = ce_client.get_cost_and_usage(
             TimePeriod={"Start": start_date, "End": end_date},
             Granularity="MONTHLY",
             Metrics=["UnblendedCost"],
             GroupBy=[
-                {"Type": "TAG", "Key": "Project"},
+                {"Type": "TAG", "Key": "Project"},  # Group by project tag
+                {"Type": "DIMENSION", "Key": "SERVICE"},  # Group by resource type
             ],
         )
-        return response
+        # Process the response and create a dictionary to store cost data
+        project_cost_data = {}
+        for group in response["ResultsByTime"][0]["Groups"]:
+            tag_key = group["Keys"][0]
+            project_cost = group["Metrics"]["UnblendedCost"]["Amount"]
+            tag_value = tag_key.split("$")[1]
+            if tag_value == "":
+                tag_value = "Others"
+            # Extract resource information
+            resource_name = group["Keys"][1]
+            resource_cost = group["Metrics"]["UnblendedCost"]["Amount"]
+            resource_type = resource_name.split("$")[0]
+            # Store data in the dictionary
+            if tag_value not in project_cost_data:
+                project_cost_data[tag_value] = {"project_cost": 0.0, "resources": {}}
+            project_cost_data[tag_value]["project_cost"] += float(project_cost)
+            project_cost_data[tag_value]["resources"][resource_name] = {
+                "resource_cost": float(resource_cost),
+                "resource_type": resource_type
+            }
+        return project_cost_data
     except Exception as e:
-        print(f"Error getting cost of project: {e}")
+        logging.error(f"Error getting cost of project: {e}")
         return None
-
-
 def lambda_handler(event, context):
-    """
-    The main function that is executed when the AWS Lambda function is triggered.
-
-    Returns:
-        str: A message indicating the success or failure of the function execution.
-    """
     try:
+        # Fetch cost data
+        project_cost_data = cost_of_project(ce_client, start_date, end_date)
+        # Create Prometheus metrics registry
         registry = CollectorRegistry()
-        g = Gauge(
+        # Create Prometheus Gauges for project spend and resource cost
+        g_resource = Gauge(
+            "Project_Resource_Cost",
+            "XC3 Project Resource Cost",
+            labelnames=["project_spend_project", "resource_name", "resource_cost"],
+            registry=registry,
+        )
+        g_project = Gauge(
             "Project_Spend_Cost",
             "XC3 Project Spend Cost",
             labelnames=["project_spend_project", "project_spend_cost"],
             registry=registry,
         )
-
-        response = cost_of_project(ce_client, start_date, end_date)
-        project_dict = {}
-
-        for group in response["ResultsByTime"][0]["Groups"]:
-            tag_key = group["Keys"][0]
-            cost = group["Metrics"]["UnblendedCost"]["Amount"]
-
-            print(f"{tag_key}: {cost}")
-            tag_value = tag_key.split("$")[1]
-            if tag_value == "":
-                tag_value = "Others"
-            print(tag_value)
-            g.labels(tag_value, cost).set(cost)
-            project_dict[tag_value] = cost
-
-        # Convert the dictionary to JSON
-        json_data = json.dumps(project_dict)
-
-        # Upload the file to S3
-        s3.put_object(
-            Bucket=os.environ["bucket_name"],
-            Key=os.environ["project_spend_prefix"],
-            Body=json_data,
-        )
-
-        push_to_gateway(
-            os.environ["prometheus_ip"], job="Project-Spend-Cost", registry=registry
-        )
+        # Iterate through project-wise cost data and populate Prometheus metrics
+        for project, data in project_cost_data.items():
+            g_project.labels(project, data["project_cost"]).set(data["project_cost"])
+            for resource_name, resource_data in data["resources"].items():
+                resource_cost = resource_data["resource_cost"]
+                g_resource.labels(project, resource_name, resource_cost).set(resource_cost)
+        # Convert data to JSON
+        json_data = json.dumps(project_cost_data)
+        # Upload data to S3 (if needed)
+        # Push data to Prometheus gateway
+        # Log success
+        logging.info("Data has been pushed to Prometheus successfully.")
         return {"statusCode": 200, "body": json_data}
-    except botocore.exceptions.ClientError as e:
-        logging.error(f"Failed to upload file to S3: {e}")
-        return {"statusCode": 500, "body": "Failed to upload file to S3."}
-
     except Exception as e:
         logging.error(f"Error executing lambda_handler: {e}")
         return "Failed to execute. Please check logs for more details."
